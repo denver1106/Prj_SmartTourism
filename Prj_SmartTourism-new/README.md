a4e2238e8773df5c1c73e0126adb82aa8881941b
linhmessi10 <dlplinh2414@clc.fitus.edu.vn>
2025-12-08 00:46:33 +0700

    Delete Prj_SmartTourism-new directory

--- Prj_SmartTourism-new/README.md
+++
@@ -1,1 +0,0 @@
-tải Database (MySQL) qua link sau: https://drive.google.com/file/d/1d-6sqqjPdQry8Bs3SJMDJxp32UCfip5Y/view?usp=sharing 
--- Prj_SmartTourism-new/config.py
+++
@@ -1,7 +0,0 @@
-﻿DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "Nhom4@36", 
-    "database": "smarttourism",
-    "port": 3306
-}
